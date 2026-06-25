527f554663c4c76e256dee0622fa5778ee9143f3
Parsa Shemirani <parsashemirani@gmail.com>
2026-06-25 11:06:08 -0700

    simplified schema to start with

--- schema.sql
+++ schema.sql
@@ -1,51 +1,17 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE files (
     sha256_hash TEXT PRIMARY KEY,
     size_bytes INTEGER NOT NULL,
     extension TEXT NOT NULL,
-    created_ts TEXT NOT NULL,
+    stats_json TEXT NOT NULL,
     inserted_ts TEXT NOT NULL, 
-
+    
+    name TEXT,
+    description TEXT,
     other_json TEXT
 );
 
-CREATE TABLE storage_devices (
-    id TEXT PRIMARY KEY,
-    name TEXT NOT NULL,
-    inserted_ts TEXT NOT NULL
-);
-
-CREATE TABLE storage_files (
-    storage_device_id TEXT NOT NULL,
-    file_sha256_hash TEXT NOT NULL,
-
-    inserted_ts TEXT NOT NULL,
-
-    PRIMARY KEY (storage_device_id, file_sha256_hash),
-
-    FOREIGN KEY (storage_device_id) REFERENCES storage_devices(id),
-    FOREIGN KEY (file_sha256_hash) REFERENCES files(sha256_hash)
-);
-
-CREATE TABLE storage_file_events (
-    id TEXT PRIMARY KEY,
-    storage_device_id TEXT NOT NULL,
-    file_sha256_hash TEXT NOT NULL,
-
-    event_type TEXT NOT NULL,
-    event_ts TEXT NOT NULL,
-
-    FOREIGN KEY (storage_device_id) REFERENCES storage_devices(id),
-    FOREIGN KEY (file_sha256_hash) REFERENCES files(sha256_hash)
-);
-
-CREATE TABLE file_instance (
-    file_sha256_hash TEXT PRIMARY KEY,
-
-    name TEXT NOT NULL,
-    inserted_ts TEXT NOT NULL,
-
-    description TEXT
-);
 
 CREATE TABLE bundles (
     id TEXT PRIMARY KEY,
@@ -64,4 +30,35 @@ CREATE TABLE bundle_files (
 
     FOREIGN KEY (bundle_id) REFERENCES bundles(id),
     FOREIGN KEY (file_sha256_hash) REFERENCES files(sha256_hash)
-)
+);
+
+CREATE TABLE directories (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    inserted_ts TEXT NOT NULL,
+    parent_id TEXT,
+
+    FOREIGN KEY (parent_id) REFERENCES directories(id)
+);
+
+CREATE TABLE directory_files (
+    directory_id TEXT NOT NULL,
+    file_sha256_hash TEXT NOT NULL,
+    inserted_ts TEXT NOT NULL,
+
+    PRIMARY KEY (directory_id, file_sha256_hash),
+
+    FOREIGN KEY (directory_id) REFERENCES directories(id),
+    FOREIGN KEY (file_sha256_hash) REFERENCES files(sha256_hash)
+);
+
+CREATE TABLE directory_bundles (
+    directory_id TEXT NOT NULL,
+    bundle_id TEXT NOT NULL,
+    inserted_ts TEXT NOT NULL,
+
+    PRIMARY KEY (directory_id, bundle_id),
+
+    FOREIGN KEY (directory_id) REFERENCES directories(id),
+    FOREIGN KEY (bundle_id) REFERENCES bundles(id)
+);
